e930cda75682061fa3805b127ec23acaf1adbc17
crackalamoo <harysdalvi@gmail.com>
2024-01-21 19:01:01 -0500

    RSA keygen

--- client.js
+++ client.js
@@ -1,11 +1,58 @@
-const P = 199933;
-const Q = 16661;
-const G = Math.floor(Math.pow(2, (P-1)/Q));
+function randomPrime(maxVal) {
+    const isPrime = (num) => {
+        for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+            if (num % i === 0) return false;
+        return num > 1;
+    }
+    let res = 1;
+    while (!isPrime(res)) {
+        res = 2 + Math.floor(Math.random() * maxVal);
+    }
+    return res;
+}
+function gcd(a, b) {
+    if (b === 0) {
+        return [a, 1, 0];
+    }
+    const [g, x, y] = gcd(b, a % b);
+    return [g, y, x - Math.floor(a/b) * y];
+}
+function modInv(a, m) {
+    return (gcd(a,m)[1] % m + m) % m;
+}
+function randomCoprime(coprimeTo, maxVal) {
+    let res = coprimeTo;
+    while (gcd(res, coprimeTo)[0] !== 1) {
+        res = 2 + Math.floor(Math.random() * maxVal);
+    }
+    return res;
+}
+
+const P = randomPrime(Math.pow(2, 12));
+const Q = randomPrime(Math.pow(2, 12));
+const N = P * Q;
+const PHI = (P-1) * (Q-1);
+const E = randomCoprime(PHI, PHI);
+const D = modInv(E, PHI);
+
+function expMod(base, exp, mod){
+    if (exp == 0)
+        return 1;
+    if (exp % 2 == 0){
+        return Math.pow(expMod(base, (exp / 2), mod), 2) % mod;
+    } else {
+        return (base * expMod(base, (exp - 1), mod)) % mod;
+    }
+}
+
+const SK = 1 + Math.floor((Q-1)*Math.random());
 
 const websocket = new WebSocket("ws://localhost:8000/");
 const username = location.port;
 
 const messages = [];
+const users = {};
+
 const messageBox = document.getElementById('message-box');
 const inputBox = document.getElementById("input-box");
 
@@ -20,22 +67,36 @@ function updateMessageBox() {
     });
 }
 
-function sendMessage(text) {
-    const message = {
-        'from': username,
-        'to': username === '3000' ? '3001' : '3000',
-        'text': text
+function connectUser(user) {
+    const event = {
+        'type': 'connect',
+        'to': user
     };
-    websocket.send(JSON.stringify({
-        'type': 'msg', ...message
-    }));
-    updateMessageBox();
+    websocket.send(JSON.stringify(event));
+}
+
+let willSend = null;
+async function sendMessage(text) {
+    const recipient = username === '3000' ? '3001' : '3000';
+    if (users[recipient] === undefined) {
+        willSend = text;
+        connectUser(recipient);
+    } else {
+        const message = {
+            'from': username,
+            'to': recipient,
+            'text': text
+        };
+        websocket.send(JSON.stringify({
+            'type': 'msg', ...message
+        }));
+        updateMessageBox();
+    }
 }
 
 function receiveData({data}) {
-    const event_str = JSON.parse(data);
-    const event = JSON.parse(event_str);
     console.log(data);
+    const event = JSON.parse(data);
     console.log(event);
     console.log(event.type);
     switch (event.type) {
@@ -47,17 +108,37 @@ function receiveData({data}) {
             });
             updateMessageBox();
             break;
+        case 'connected':
+            users[event.to] = {
+                'pk_n': event.pk_n,
+                'pk_e': event.pk_e
+            };
+            if (willSend !== null) {
+                sendMessage(willSend);
+                willSend = null;
+            }
+            break;
         default:
             break;
     }
 }
 websocket.addEventListener("message", receiveData);
 
+function login() {
+    const event = {
+        'type': 'login',
+        'user': username,
+        'pk_n': N,
+        'pk_e': E
+    };
+    websocket.send(JSON.stringify(event));
+}
+websocket.addEventListener("open", login);
+
 function typeText(event) {
     let key = event.keyCode;
     if (key === 13) {
         event.preventDefault();
-        console.log(inputBox.value);
         sendMessage(inputBox.value);
         inputBox.value = '';
     }
--- server.py
+++ server.py
@@ -3,14 +3,29 @@ import websockets
 import json
 
 CONNECTIONS = set()
+USERS = {}
 
 async def handler(websocket):
     CONNECTIONS.add(websocket)
     async for message in websocket:
         print("RECEIVED MESSAGE")
+        message = json.loads(message)
         print(message)
-        for connection in CONNECTIONS:
-            await connection.send(json.dumps(message))
+        if message['type'] == 'msg':
+            for connection in CONNECTIONS:
+                await connection.send(json.dumps(message))
+        elif message['type'] == 'login':
+            USERS[message['user']] = {
+                'pk_n': message['pk_n'],
+                'pk_e': message['pk_e']
+            }
+        elif message['type'] == 'connect':
+            await websocket.send(json.dumps({
+                'type': 'connected',
+                'to': message['to'],
+                'pk_n': USERS[message['to']]['pk_n'],
+                'pk_e': USERS[message['to']]['pk_e']
+            }))
 
 async def main():
     async with websockets.serve(handler, '', 8000):
